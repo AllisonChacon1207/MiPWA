b24bd836ce1465dd81dd8de09fcb69ac93ec371c
AllisonChacon1207 <62119402+AllisonChacon1207@users.noreply.github.com>
2025-02-27 08:45:19 -0600

    Depuracion de codigo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,47 +4,54 @@
       <q-toolbar>
         <q-toolbar-title>Registro Vehicular</q-toolbar-title>
         <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+        <!--------------------------------------------------------------------------------->
         <!-- Botón de instalación -->
         <q-btn v-if="showInstallButton" @click="installPWA" label="Instalar App" color="positive" />
+        <!--------------------------------------------------------------------------------->
       </q-toolbar>
     </q-header>
-
+    <!------------------------------------------------------------------------------------------------------------->
     <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400">
+      <!------------------------------------------------------------------------------------------------------------->
       <q-scroll-area
         style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd"
       >
         <q-list padding>
-          <q-item clickable v-ripple to="/">
+          <!------------------------------------------------------------------------------------------------------------->
+          <q-item clickable v-ripple to="/"
+            ><!--------------------------------------------------------------->
             <q-item-section avatar>
               <q-icon name="inbox" />
             </q-item-section>
-            <q-item-section>Inicio</q-item-section>
+            <q-item-section> Inicio </q-item-section>
           </q-item>
-
-          <q-item active clickable v-ripple to="/MiPrimero">
+          <!------------------------------------------------------------------------------------------------------------->
+          <q-item active clickable v-ripple to="/MiPWA/src/pages/MiPrimero.vue">
             <q-item-section avatar>
               <q-icon name="star" />
             </q-item-section>
-            <q-item-section>Mi Datos en QR</q-item-section>
+            <q-item-section> Mi Datos en QR </q-item-section>
           </q-item>
-
-          <q-item clickable v-ripple to="/MiSegundo">
+          <!------------------------------------------------------------------------------------------------------------->
+          <q-item clickable v-ripple to="/MiPWA/src/pages/MiSegundo.vue">
             <q-item-section avatar>
               <q-icon name="send" />
             </q-item-section>
-            <q-item-section>Mi NFC</q-item-section>
+            <q-item-section> Mi NFC </q-item-section>
           </q-item>
-
+          <!------------------------------------------------------------------------------------------------------------->
           <q-item clickable v-ripple>
             <q-item-section avatar>
               <q-icon name="drafts" />
             </q-item-section>
-            <q-item-section>DATOS API</q-item-section>
-          </q-item>
-        </q-list>
+            <q-item-section> DATOS API </q-item-section>
+          </q-item> </q-list
+        ><!------------------------------------------------------------------------------------------>
       </q-scroll-area>
-
-      <q-img class="absolute-top" style="height: 150px; background-color: #09989c">
+      <!------------------------------------------------------------------------------------->
+      <!------------------------------------------------------------------------------------------------------------->
+      <q-img class="absolute-top" style="height: 150px; background-color: #09989c"
+        ><!----------COLOR DE LA PALETA DE PERFIL-------------->
         <div class="absolute-bottom bg-transparent">
           <q-avatar size="56px" class="q-mb-sm">
             <img src="/src-pwa/carro.png" />
@@ -54,13 +61,15 @@
         </div>
       </q-img>
     </q-drawer>
-
+    <!------------------------------------------------------------------------------------------------------------->
     <q-page-container>
-      <router-view />
+      <router-view> </router-view>
     </q-page-container>
   </q-layout>
 </template>
-
+<!--------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------->
+<!------------------------------------------------------------------------------------------------------------->
 <script>
 import { ref, onMounted } from 'vue'
 
