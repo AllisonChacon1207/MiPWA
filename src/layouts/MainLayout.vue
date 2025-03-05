6d577dfa2c0e0decad7303eac9c7f0242ac99721
Fabiola Flores <allisonchaconflores@gmail.com>
2025-03-05 11:50:46 -0600

    Descripción de los cambios

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-toolbar-title>REGISTRO VEHICULAR</q-toolbar-title>
         <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
-        <!--------------------------------------------------------------------------------->
+        <!--------------------------------------------------fdgfgdfgdfdfgdfgd------------------------------->
         <div>
           <!-- Botón de instalación PWA -->
           <q-btn v-if="showInstallButton" @click="installPWA" label="Instalar App" color="positive" />
@@ -60,7 +60,7 @@
           <q-avatar size="56px" class="q-mb-sm">
             <img src="https://avatars.githubusercontent.com/u/62119402?s=400&v=4">
           </q-avatar>
-          <div class="text-weight-bold">Allison Fabiola Flores</div>
+          <div class="text-weight-bold">Fabiola Flores</div>
           <div>Registro Vehicular</div>
         </div>
       </q-img>
