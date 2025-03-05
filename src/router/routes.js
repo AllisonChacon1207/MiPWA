657060fea41cb407003cbdab5ff48b12abdaa4fa
Fabiola Flores <allisonchaconflores@gmail.com>
2025-03-05 10:23:53 -0600

    Inicio del Repositorio

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }, // Ruta principal
+      { path: '/MiPWA/src/pages/MiDato.vue', component: () => import('src/pages/MiDato.vue') }, // Ruta para MiPrimero.vue
+      { path: '/MiPWA/src/pages/MiLector.vue', component: () => import('src/pages/MiLector.vue') }, // Ruta para MiSegundo.vue
+      { path: '/MiPWA/src/pages/TablaLibros.vue', component: () => import('src/pages/TablaLibros.vue') }, // Ruta para TablaLibros.vue
+
+    ],
+  },
+
+  // Siempre deja esta última ruta para manejar rutas no encontradas (404)
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+
+];
+
+export default routes;
